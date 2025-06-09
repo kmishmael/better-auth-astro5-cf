1cd457b02d020d54fb90b468bf04b2e67af0d743
kmishmael <kmishmael@gmail.com>
2025-06-09 22:55:55 +0300

    remix dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,9 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+	 platformProxy: {
+      enabled: true,
+    },
+  }),
 })
